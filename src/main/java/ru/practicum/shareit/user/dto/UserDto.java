ed99ff6ea32a4fc9bf6550042ef286f65818ffa1
isthatkirill <kirill.emelyanoff2021@yandex.ru>
2023-07-04 14:06:03 +0300

    test: tests added for user

--- src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -1,9 +1,6 @@
 package ru.practicum.shareit.user.dto;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 import javax.validation.constraints.Email;
@@ -12,6 +9,7 @@ import javax.validation.constraints.NotNull;
 
 @Data
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserDto {
---
+++ src/test/java/ru/practicum/shareit/user/dto/UserDtoTest.java
@@ -0,0 +1,49 @@
+package ru.practicum.shareit.user.dto;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@JsonTest
+class UserDtoTest {
+
+    @Autowired
+    private JacksonTester<UserDto> json;
+
+    private final UserDto userDto = UserDto.builder()
+            .id(1L)
+            .email("testemail@yahoo.com")
+            .name("testName")
+            .build();
+
+    @Test
+    @SneakyThrows
+    void testUserDto() {
+        JsonContent<UserDto> result = json.write(userDto);
+
+        assertThat(result)
+                .hasJsonPathNumberValue("$.id", 1L)
+                .hasJsonPathStringValue("$.email", "testemail@yahoo.com")
+                .hasJsonPathStringValue("$.name", "testName");
+    }
+
+    @Test
+    @SneakyThrows
+    void testUserDtoWithNullFields() {
+        userDto.setName(null);
+        userDto.setEmail(null);
+        JsonContent<UserDto> result = json.write(userDto);
+
+        assertThat(result)
+                .hasJsonPathNumberValue("$.id", 1L)
+                .hasEmptyJsonPathValue("$.email")
+                .hasEmptyJsonPathValue("$.name");
+    }
+
+}
